4482fffab7ea6febb71a0152da32ba3e2e45519d
pradip pachapole <pachapolp2932002@gmail.com>
2025-11-01 12:41:08 +0530

    Implement Jenkins pipeline in helloworld.py
    Added a Jenkins pipeline configuration with stages for dev, test, and prod environments.

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,25 @@
-print ('hello world')
+pipeline { 
+     agent any 
+  
+     stages { 
+         stage('dev') { 
+             steps { 
+                 echo 'I am in dev' 
+                 sh 'git --version' 
+             } 
+         } 
+         stage('test') { 
+             steps { 
+                 echo 'I am in test' 
+                 sh 'docker --version' 
+             } 
+         } 
+         stage('prod') { 
+             steps { 
+                 echo 'I am in prod' 
+                 sh 'pwd' 
+                 sh 'git init' 
+             } 
+         } 
+     } 
+ }
